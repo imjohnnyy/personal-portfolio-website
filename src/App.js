b2a1c4a46962ec688a305c83914e96293e10a475
imjohnnyy <ldong4056@gmail.com>
2022-11-18 22:32:51 +1300

    setup routes to Home and Projects pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
