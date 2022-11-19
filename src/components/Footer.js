e763805920baf3d3f93e7fcb3b586561df181e97
imjohnnyy <ldong4056@gmail.com>
2022-11-19 14:20:52 +1300

    styled the social media icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,18 @@
+import React from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import EmailIcon from '@mui/icons-material/Email';
+import "../styles/Footer.css"
 
 function Footer() {
   return (
     <div className="footer">
       <div>
-        <GitHubIcon />
-        <LinkedInIcon />
-        <EmailIcon />
+        <GitHubIcon  style={{ fontSize: 58, color:"white", margin: 20 }}/>
+        <LinkedInIcon  style={{ fontSize: 60, color:"white", margin: 20 }}/>
+        <EmailIcon  style={{ fontSize: 60, color:"white", margin: 20 }}/>
       </div>
-      <p> &copy; 2022 johnnydong.me</p>
+      <p> &copy;2022 johnnydong.me</p>
     </div>
   );
 }
