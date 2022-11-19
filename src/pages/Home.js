afd15a86434d735b731e2716f1411246e5123154
imjohnnyy <ldong4056@gmail.com>
2022-11-19 16:56:09 +1300

    added project item components and styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,18 @@
 import React from "react";
-import '../styles/Home.css';
+import "../styles/Home.css";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 function Home() {
   return (
     <div className="home">
       <div className="about">
-        <h2>Hi, my name is Johnny.</h2>
+        <h2>Hi, I'm Johnny</h2>
+        <div className="location">
+          <div className="location-svg">
+            <LocationOnIcon />
+          </div>
+          <h3>Auckland, &nbsp;New Zealand</h3>
+        </div>
         <div className="prompt">
           <p>
             I am an aspiring Software Developer with a passion for learning and
@@ -18,7 +25,9 @@ function Home() {
         <ol className="list">
           <li className="item">
             <h2>Front-end</h2>
-            <span>ReactJS, React Native, HTML, CSS, BootStrap, Material UI</span>
+            <span>
+              ReactJS, React Native, HTML, CSS, BootStrap, Material UI
+            </span>
           </li>
           <li className="item">
             <h2>Back-end</h2>
@@ -26,7 +35,7 @@ function Home() {
           </li>
           <li className="item">
             <h2>Languages</h2>
-            <span>JavaScript, Python</span>
+            <span>JavaScript, PHP, Python</span>
           </li>
         </ol>
       </div>
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,16 @@
+import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import CabsPro from '../assets/CabsPro Website.png'
+import "../styles/Projects.css";
+
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className="projects">
+      <h1>My Personal Projects</h1>
+      <div className="projectList">
+        <ProjectItem name="CabsPro" image={CabsPro} />
+      </div>
+    </div>
   )
 }
 export default Projects
