ce3c89a4914e4cc47afeec67510fa420ecee5ada
imjohnnyy <ldong4056@gmail.com>
2022-11-19 13:47:25 +1300

    added toggle functionality to show navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,28 @@
-import {Link} from 'react-router-dom';
-import "../styles/Navbar.css"
-import  ReorderIcon  from '@material-ui/icons/Reorder';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
 
 function Navbar() {
+  const [isExpandNavbar, setIsExpandNavbar] = useState(false);
+
   return (
-    <div className="navbar">
-        <div className="toggleButton">
-            <button><ReorderIcon /></button>
-        </div>
-        <div className="links">
-            <Link to="/">Home</Link>
-            <Link to="/projects">Projects</Link>
-        </div>
+    <div className="navbar" id={isExpandNavbar ? "open" : "close"}>
+      <div className="toggleButton">
+        <button
+          onClick={() => {
+            setIsExpandNavbar((prev) => !prev);
+          }}
+        >
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+      </div>
     </div>
-  )
+  );
 }
 
-
-export default Navbar
+export default Navbar;
