72943e5b4d3fdb2647a89360685d8c15849f8674
imjohnnyy <ldong4056@gmail.com>
2022-11-19 17:07:13 +1300

    Dynamically displaying projects feature added

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,13 @@
+import CabsPro from "../assets/CabsPro.png";
+import WeatherApp from "../assets/WeatherApp.png"
+
+export const ProjectList = [
+  {
+    name: "CabsPro",
+    image: CabsPro
+  },
+  {
+    name: "Weather Forecast App",
+    image: WeatherApp
+  }
+];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,16 +1,18 @@
 import React from "react";
 import ProjectItem from "../components/ProjectItem";
-import CabsPro from '../assets/CabsPro Website.png'
 import "../styles/Projects.css";
+import { ProjectList } from "../helpers/ProjectList";
 
 function Projects() {
   return (
     <div className="projects">
       <h1>My Personal Projects</h1>
       <div className="projectList">
-        <ProjectItem name="CabsPro" image={CabsPro} />
+        {ProjectList.map((project) => {
+          return <ProjectItem name={project.name} image={project.image} />;
+        })}
       </div>
     </div>
-  )
+  );
 }
-export default Projects
+export default Projects;
