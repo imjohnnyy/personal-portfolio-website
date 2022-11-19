b6ee3029771baedefc7a6b490e04cb8b527c9e74
imjohnnyy <ldong4056@gmail.com>
2022-11-19 14:34:29 +1300

    added content and styling to home page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Projects from "./pages/Projects";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+
 function App() {
   return (
     <div className="App">
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,36 @@
+import React from "react";
+import '../styles/Home.css';
+
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="home">
+      <div className="about">
+        <h2>Hi, my name is Johnny.</h2>
+        <div className="prompt">
+          <p>
+            I am an aspiring Software Developer with a passion for learning and
+            building software!
+          </p>
+        </div>
+      </div>
+      <div className="skills">
+        <h1>Skills</h1>
+        <ol className="list">
+          <li className="item">
+            <h2>Front-end</h2>
+            <span>ReactJS, React Native, HTML, CSS, BootStrap, Material UI</span>
+          </li>
+          <li className="item">
+            <h2>Back-end</h2>
+            <span>MySQL, NodeJS, ExpressJS, MongoDB</span>
+          </li>
+          <li className="item">
+            <h2>Languages</h2>
+            <span>JavaScript, Python</span>
+          </li>
+        </ol>
+      </div>
+    </div>
+  );
 }
-export default Home
+export default Home;
